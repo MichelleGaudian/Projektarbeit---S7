c53a039204a0c994d77ea63d6649eb759d8a1c36
Michelle Gaudian <consulting@sevian7.com>
2023-03-11 19:37:00 +0100

    Triangel für nav angepasst und eingefärbt
    html überschriften angepasst

--- assets/JS/scripts.js
+++ assets/JS/scripts.js
@@ -44,15 +44,58 @@ window.addEventListener('resize', function(){
   
 
 // bilder 4 stück einrücken
+/*
 $(window).scroll(function() {
     var scrollBottom = $(window).scrollTop() + $(window).height();
-    var containerTop = $(".image-container").offset().top;
+    var columnsTop = $(".columns").offset().top;
     
-    if (scrollBottom > containerTop) {
-      $(".image-container img").each(function(index) {
+    if (scrollBottom > columnsTop) {
+      $(".content-container img-container").each(function(index) {
         $(this).delay(index * 250).animate({opacity: 1}, 1000);
       });
       
-      $(".image-container h3, .image-container p").delay(1000).animate({opacity: 1}, 1000);
+      $(".content-container h3, .content-container p").delay(1000).animate({opacity: 1}, 1000);
     }
   });
+  */
+// Select the columns container element
+const columns = document.querySelector('.columns');
+
+// Define a function to check if an element is in view
+function isInViewport(element) {
+  const rect = element.getBoundingClientRect();
+  return (
+    rect.left >= 0 &&
+    rect.top >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+// Define a function to fade in an element
+function fadeIn(element) {
+  let opacity = 0;
+  const timer = setInterval(() => {
+    if (opacity >= 1) {
+      clearInterval(timer);
+    }
+    element.style.opacity = opacity;
+    opacity += 0.1;
+  }, 50);
+}
+
+// Add an event listener to the window object to check for scroll events
+window.addEventListener('scroll', () => {
+  // If the columns container is in view, fade in each column
+  if (isInViewport(columns)) {
+    const columnElements = columns.querySelectorAll('.content-container');
+    columnElements.forEach((column, index) => {
+      setTimeout(() => {
+        fadeIn(column);
+      }, index * 200);
+    });
+  }
+});
+
+
+
