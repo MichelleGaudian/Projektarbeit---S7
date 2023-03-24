bb72fb73d0c9c36d05ff729bdac86a7d93db7216
Michelle Gaudian <consulting@sevian7.com>
2023-03-24 12:14:28 +0100

    404 angepasst

--- s7theme/404.php
+++ s7theme/404.php
@@ -1,6 +1,5 @@
 <?php get_header( ); ?>
 <main id="content" class="container">
-	/* MGX */
     <h1><?php _e('Seite nicht gefunden', 'michelle');  ?></h1>
     <h2><?php _e('Fehler 404', 'michelle'); ?></h2>
     <a href="<?php echo home_url( ); ?>" class="btn"><?php _e('Zur Startseite', 'michelle'); ?></a>
